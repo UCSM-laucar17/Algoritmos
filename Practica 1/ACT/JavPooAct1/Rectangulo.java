55cefa96fe4d29ad398c27540827c8e9102e51d6
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-03-18 17:35:00 -0500

    Update Rectangulo.java

--- Practica 1/ACT/JavPooAct1/Rectangulo.java
+++ Practica 1/ACT/JavPooAct1/Rectangulo.java
@@ -2,8 +2,8 @@ class Rectangulo{
     Coordenada esquina1;
     Coordenada esquina2;
     public Rectangulo(Coordenada c1,Coordenada c2){
-        esquina1=new Coordenada(c1); //copia c1
-        esquina2=new Coordenada(c2); //copia c2
+        esquina1=new Coordenada(c1); //crea c1
+        esquina2=new Coordenada(c2); //crea c2
     }
     //retorma e1
     Coordenada getEsquina1(){
@@ -19,6 +19,7 @@ class Rectangulo{
     void setEsquina2(Coordenada c){
         esquina2=new Coordenada(c);
     }
+    //retorna las coor [,]
     public String toString(){
         return "["+esquina1+" , "+esquina2+"]";
     }
