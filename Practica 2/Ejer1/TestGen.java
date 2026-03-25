268ecc659bc7f3eb16a97ec8364ec9b68733f501
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-03-25 17:12:14 -0500

    Update TestGen.java

--- Practica 2/Ejer1/TestGen.java
+++ Practica 2/Ejer1/TestGen.java
@@ -1,11 +1,9 @@
-public class TestGen {
-    public static void main(String[] args) {
-    
-    String[] v = {"Perez", "Sanchez", "Rodriguez"};
-    Integer[] w = {12, 34, 56};
-
-    System.out.println(exist(v, "Sanchez"));    // true
-    System.out.println(exist(w, 34));           //true
-    // System.out.println(exist(w, "Salas"));   // Error intencional (tipos incompatibles)
-    }
+public class TestGen{
+    public static void main(String[]args){
+        String[] v={"Kana", "Paredes", "Alarcon", "Alvarez"};
+        Integer[] w={12,34,56};
+        System.out.println(DemoMetodosGenericos.exist(v, "Alvarez"));
+        System.out.println(DemoMetodosGenericos.exist(w, "34"));
+        System.out.println(DemoMetodosGenericos.exist(v, "12"));
+   }
 }
