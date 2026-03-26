aed063656861aeaa91874f1072fd5e943d3c3a28
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-03-25 21:45:58 -0500

    Create Cajoneria.java

---
+++ Practica 2/Ejer3/Cajoneria.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList; 
+
+public class Cajoneria<T>{ 
+    private ArrayList<Caja<T>> lista; 
+    public Cajoneria(){
+        lista = new ArrayList<>(); 
+    } 
+    public void add(Caja<T> caja){ 
+        lista.add(caja); 
+    } 
+    public ArrayList<Caja<T>> getLista(){ 
+        return lista; 
+    } 
+}  
