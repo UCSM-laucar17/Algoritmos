58cc5a6e40d02b9fc639d4cda0a50f28df10bb34
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-04-29 17:13:56 -0500

    Create Ejer_06.java

---
+++ Practica 5/Ejer6/Ejer_06.java
@@ -0,0 +1,19 @@
+public class Ejer_06 {
+    public static <T> ListLinked<T> concatenarListas(ListLinked<T> lista1, ListLinked<T> lista2) {
+        ListLinked<T> nuevaLista = new ListLinked<>();
+        if (lista1 != null && !lista1.isEmptyList()) {
+            copiarElementos(lista1, nuevaLista);
+        }
+        if (lista2 != null && !lista2.isEmptyList()) {
+            copiarElementos(lista2, nuevaLista);
+        }        
+        return nuevaLista;
+    }
+    private static <T> void copiarElementos(ListLinked<T> origen, ListLinked<T> destino) {
+        Node<T> actual = origen.getFirst();
+        while (actual != null) {
+            destino.insertLast(actual.value);
+            actual = actual.next;
+        }
+    }
+}
