aa96a3771f51663e25c4584fa7580651ef8816d6
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-04-29 17:13:41 -0500

    Create ContarNodos.java

---
+++ Practica 5/Ejer4/ContarNodos.java
@@ -0,0 +1,11 @@
+public class ContarNodos{
+    public static <T> int contarNodo(NodoLista<T> head){
+        int c=0;
+        NodoLista<T> actual=head;
+        while(actual!=null){
+            c++;
+            actual=actual.siguiente;
+        }
+        return c;
+    }
+}
