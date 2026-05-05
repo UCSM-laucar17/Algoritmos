fbb87c5864a8692ac61a22db5a615c39b21a666e
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 15:29:53 -0500

    Create Deque.java

---
+++ Practica 6/Act2/Deque.java
@@ -0,0 +1,10 @@
+import Act1.ExceptionIsEmpty;
+public interface Deque<E> {
+    void addFirst(E x);
+    void addLast(E x);
+    E removeFirst() throws ExceptionIsEmpty;
+    E removeLast() throws ExceptionIsEmpty;
+    E getFirst() throws ExceptionIsEmpty;
+    E getLast() throws ExceptionIsEmpty;
+    boolean isEmpty();
+}
