e691d8de367f3c04eaf58b8a34eb2925deb6072f
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 15:29:49 -0500

    Create QueueArray.java

---
+++ Practica 6/Act1/QueueArray.java
@@ -0,0 +1,49 @@
+public class QueueArray<E> implements Queue<E> {
+    private E[] array;
+    private int front;
+    private int rear;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public QueueArray(int n) {
+        array = (E[]) new Object[n];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    public void enqueue(E x) {
+        if (isFull()) throw new RuntimeException();
+        rear = (rear + 1) % array.length;
+        array[rear] = x;
+        size++;
+    }
+
+    public E dequeue() throws ExceptionIsEmpty {
+        if (isEmpty()) throw new ExceptionIsEmpty("");
+        E aux = array[front];
+        front = (front + 1) % array.length;
+        size--;
+        return aux;
+    }
+
+    public E front() throws ExceptionIsEmpty {
+        if (isEmpty()) throw new ExceptionIsEmpty("");
+        return array[front];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == array.length;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < size; i++)
+            s += array[(front + i) % array.length] + " ";
+        return s;
+    }
+}
