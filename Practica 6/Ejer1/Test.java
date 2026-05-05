814882b79d6d22501cb680d5347b1371c36fb446
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 15:29:39 -0500

    Create Test.java

---
+++ Practica 6/Ejer1/Test.java
@@ -0,0 +1,13 @@
+import Act1.ExceptionIsEmpty;
+public class Test{
+    public static void main(String[]args)throws ExceptionIsEmpty{
+        StackLink<Integer>pila = new StackLink<>();
+        pila.push(10);
+        pila.push(20);
+        pila.push(30);
+        System.out.println(pila);
+        System.out.println(pila.pop());
+        System.out.println(pila.top());
+        System.out.println(pila);
+    }
+}
