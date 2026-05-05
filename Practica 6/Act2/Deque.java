f73688dada6c319464ee12e6aab7bffbc7fb2420
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 16:29:59 -0500

    Update Deque.java

--- Practica 6/Act2/Deque.java
+++ Practica 6/Act2/Deque.java
@@ -1,12 +1,12 @@
 package Act2;
 
 import Act1.ExceptionIsEmpty;
-public interface Deque<E> {
-    void addFirst(E x);
-    void addLast(E x);
-    E removeFirst() throws ExceptionIsEmpty;
-    E removeLast() throws ExceptionIsEmpty;
-    E getFirst() throws ExceptionIsEmpty;
-    E getLast() throws ExceptionIsEmpty;
-    boolean isEmpty();
+public interface Deque<E> { //interfaz metodos de un Deque
+    void addFirst(E x); //aniadir al inicio
+    void addLast(E x); //aniadir al fin
+    E removeFirst() throws ExceptionIsEmpty; //quitar el inicio
+    E removeLast() throws ExceptionIsEmpty; //quitar el fin 
+    E getFirst() throws ExceptionIsEmpty; //obtener el inicio
+    E getLast() throws ExceptionIsEmpty; //obtener el fin
+    boolean isEmpty(); //ver si esta vacio
 }
