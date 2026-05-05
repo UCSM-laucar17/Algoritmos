ab1a845331caf8870ed6d64dc0635babd3bbc057
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 15:29:42 -0500

    Create StackLink.java

---
+++ Practica 6/Ejer1/StackLink.java
@@ -0,0 +1,40 @@
+import Act1.ExceptionIsEmpty;
+
+class StackLink<E> implements Stack<E>{
+    private Node<E> top;
+    public StackLink(){
+        this.top = null;
+    }
+
+    public void push(E x){
+        Node<E>n = new Node<>(x);
+        n.setNext(top);
+        top = n;
+    }
+
+    public E pop() throws ExceptionIsEmpty{
+        if (isEmpty()) throw new ExceptionIsEmpty("");
+        E aux = top.getData();
+        top = top.getNext();
+        return aux;
+    }
+
+    public E top() throws ExceptionIsEmpty{
+        if (isEmpty()) throw new ExceptionIsEmpty("");
+        return top.getData();
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public String toString(){
+        String s = "";
+        Node<E>aux = top;
+        while (aux != null){
+            s += aux.getData()+ " ";
+            aux = aux.getNext();
+        }
+        return s;
+    }
+}
