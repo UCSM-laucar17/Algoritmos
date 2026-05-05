70b0cc4371fb0ff542ef8f0003f5e01904eb2812
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 16:26:28 -0500

    Create TestQueue.java

---
+++ 1/TestQueue.java
@@ -0,0 +1,23 @@
+package actividad1;
+
+public class TestQueue{
+    public static void main(String[] args) throws ExceptionIsEmpty{
+        QueueArray<Integer>colaInt = new QueueArray<>(5);
+        colaInt.enqueue(10);
+        colaInt.enqueue(20);
+        colaInt.enqueue(30);
+        System.out.println(colaInt);
+        System.out.println(colaInt.dequeue());
+        System.out.println(colaInt.front());
+        System.out.println(colaInt);
+
+        QueueArray<String> colaStr = new QueueArray<>(5);
+        colaStr.enqueue("Ana");
+        colaStr.enqueue("Pedro");
+        colaStr.enqueue("Juan");
+        System.out.println(colaStr);
+        System.out.println(colaStr.dequeue());
+        System.out.println(colaStr.front());
+        System.out.println(colaStr);
+    }
+}
