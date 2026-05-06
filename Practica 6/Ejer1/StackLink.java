b754d1a2badf317e6bb1f986eec4f2ae1af987ab
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:18:33 -0500

    Update StackLink.java

--- Practica 6/Ejer1/StackLink.java
+++ Practica 6/Ejer1/StackLink.java
@@ -6,29 +6,24 @@ public class StackLink<E> implements Stack<E>{
     public StackLink(){
         this.top = null;
     }
-
     public void push(E x){
         Node<E>n = new Node<>(x);
         n.setNext(top);
         top = n;
     }
-
     public E pop() throws ExceptionIsEmpty{
         if (isEmpty()) throw new ExceptionIsEmpty("");
         E aux = top.getData();
         top = top.getNext();
         return aux;
     }
-
     public E top() throws ExceptionIsEmpty{
         if (isEmpty()) throw new ExceptionIsEmpty("");
         return top.getData();
     }
-
     public boolean isEmpty(){
         return top == null;
     }
-
     public String toString(){
         String s = "";
         Node<E>aux = top;
