70631b60a3ae57db28005ea27dab0f5913e68921
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:03:59 -0500

    Delete TestGen.java

--- Practica 2/Ejer1/TestGen.java
+++
@@ -1,9 +0,0 @@
-public class TestGen{
-    public static void main(String[]args){
-        String[] v={"Kana", "Paredes", "Alarcon", "Alvarez"};
-        Integer[] w={12,34,56};
-        System.out.println(DemoMetodosGenericos.exist(v, "Alvarez"));
-        System.out.println(DemoMetodosGenericos.exist(w, "34"));
-        System.out.println(DemoMetodosGenericos.exist(v, "12"));
-   }
-}
