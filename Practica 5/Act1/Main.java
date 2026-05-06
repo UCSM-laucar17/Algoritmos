204a0ad5bcd5f9f244c699aba342edef8258347a
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:06:07 -0500

    Create Main.java

---
+++ Practica 5/Act1/Main.java
@@ -0,0 +1,9 @@
+package Act1;
+
+public class Main{
+    public static void main(String[] args){
+        Node<Integer> nodo = new Node<>(42);
+        System.out.println(nodo.value);//42
+        System.out.println(nodo.next);//null
+    }
+} 
