b2b7d4bcba0fa989d1c6890e39e6a3ec702d1b18
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:06:45 -0500

    Update ExceptionIsEmpty.java

--- Practica 6/Act1/ExceptionIsEmpty.java
+++ Practica 6/Act1/ExceptionIsEmpty.java
@@ -1,6 +1,7 @@
 package Act1;
+//clase para solo la excepcion
 public class ExceptionIsEmpty extends Exception{
     public ExceptionIsEmpty(String message){
-        super(message);
+        super(message); //llama al constructor de Exception
     }
 }
