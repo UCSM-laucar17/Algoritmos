e3a9f7df6ab6341e7084b950ff2bcb93833599f3
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:05:08 -0500

    Create Chocolatina.java

---
+++ Practica 2/Ejercicio/Chocolatina.java
@@ -0,0 +1,17 @@
+public class Chocolatina{ 
+    private String marca; 
+    public Chocolatina(String marca){ 
+        this.marca = marca; 
+    } 
+    public String getMarca(){ 
+        return marca; 
+    } 
+    public void setMarca(String marca){ 
+        this.marca = marca; 
+    } 
+    @Override 
+    public String toString(){ 
+        return "Marca: " + marca; 
+    } 
+}  
+
