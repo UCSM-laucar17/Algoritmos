de39df6a38f50cc1236c9e3fd5d5ac06fba26753
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:06:09 -0500

    Create Main.java

---
+++ Practica 5/Act2/Main.java
@@ -0,0 +1,8 @@
+package Act2;
+
+public class Main {
+    public static void main(String[] args){
+        Tarea t = new Tarea("Deploy produccion", 1, "pendiente");
+        System.out.println(t); 
+    }
+}
