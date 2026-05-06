607dd8c42305f8d63aad7f65eb79c65b8f179296
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:05:20 -0500

    Create Permutaciones.java

---
+++ Practica 4/Act2/Permutaciones.java
@@ -0,0 +1,24 @@
+package Act2;
+import java.util.*;
+public class Permutaciones{
+    public static void permutar(int[] arr, List<Integer> actual, boolean[] usado){
+        if (actual.size() == arr.length){
+            System.out.println(actual);
+            return;
+        }
+        for (int i = 0; i < arr.length; i++){
+            if (!usado[i]) {
+                usado[i] = true;
+                actual.add(arr[i]);
+                permutar(arr, actual, usado);
+                actual.remove(actual.size() - 1); //Backtracking
+                usado[i] = false;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3};
+        boolean[] usado = new boolean[arr.length];
+        permutar(arr, new ArrayList<>(), usado);
+    }
+}
