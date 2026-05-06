bddca5d561632af1155d6980468275851f7caceb
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-05 22:03:26 -0500

    Create Ejer1.java

---
+++ Practica 1/Ejer1/Ejer1.java
@@ -0,0 +1,10 @@
+public class Ejer1{
+    public static void main(String[] args){
+        ContainerRect c=new ContainerRect(5);
+        Rectangulo r1=new Rectangulo(new Coordenada(1.5,0.3),new Coordenada(7.6,2.2));
+        Rectangulo r2=new Rectangulo(new Coordenada(4.0,4.2),new Coordenada(9.4,-2.5));
+        c.addRectangulo(r1);
+        c.addRectangulo(r2);
+        System.out.println(c);
+    }
+}
