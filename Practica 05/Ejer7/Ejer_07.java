18c60be1f79ce57dc304e9c4bdadb55c0c21a1e9
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-11 18:10:30 -0500

    Create Ejer_07.java

---
+++ Practica 05/Ejer7/Ejer_07.java
@@ -0,0 +1,18 @@
+package Ejer7;
+
+import Act1.Node;
+import Act3.ListLinked;
+
+public class Ejer_07 {
+    public static <T> ListLinked<T> invertirLista(ListLinked<T> lista) {
+        ListLinked<T> nuevaLista = new ListLinked<>();        
+        if (lista != null && !lista.isEmptyList()) {
+            Node<T> actual = lista.getFirst();            
+            while (actual != null) {
+                nuevaLista.insertFirst(actual.value);
+                actual = actual.next;
+            }
+        }       
+        return nuevaLista;
+    }
+}
