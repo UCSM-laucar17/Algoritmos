0e0d53d966032fcac48406c94e4183252d623b85
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-11 18:12:52 -0500

    Delete Main.java

--- Practica 5/Act2/Main.java
+++
@@ -1,8 +0,0 @@
-package Act2;
-
-public class Main {
-    public static void main(String[] args){
-        Tarea t = new Tarea("Deploy produccion", 1, "pendiente");
-        System.out.println(t); 
-    }
-}
