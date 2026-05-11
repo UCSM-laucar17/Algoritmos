927aeecdc2a9d3c87a3a7642ba3de7769d8c1b03
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-11 18:14:02 -0500

    Delete Stack.java

--- Practica 6/Ejer1/Stack.java
+++
@@ -1,9 +0,0 @@
-package Ejer1;
-import Act1.ExceptionIsEmpty;
-
-public interface Stack<E>{
-    void push(E x);
-    E pop() throws ExceptionIsEmpty;
-    E top() throws ExceptionIsEmpty;
-    boolean isEmpty();
-}
