5521d3b35a578fe52bb0a4f3b2ad01faa5a643d3
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-11 19:16:17 -0500

    Create BinarySearchTree.java

---
+++ Practica 07/Act05/BinarySearchTree.java
@@ -0,0 +1,7 @@
+import Act04;
+public interface BinarySearchTree<E extends Comparable<E>>{
+    void insert(E data) throws ItemDuplicated;
+    E search(E data) throws ItemNoFound;
+    void delete(E data) throws ExceptionIsEmpty;
+    boolean isEmpty();
+}
