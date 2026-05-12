49227b253db41792151ad9618dea3de83e145347
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-12 10:17:13 -0500

    Delete BinarySearchTree.java

--- Practica 07/Act05/BinarySearchTree.java
+++
@@ -1,7 +0,0 @@
-import Practica.Act04;
-public interface BinarySearchTree<E extends Comparable<E>>{
-    void insert(E data) throws ItemDuplicated;
-    E search(E data) throws ItemNoFound;
-    void delete(E data) throws ExceptionIsEmpty;
-    boolean isEmpty();
-}
