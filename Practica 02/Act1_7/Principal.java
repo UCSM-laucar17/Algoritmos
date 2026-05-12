3e21764c284b6c40175b736558cd8d8966e72203
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-12 10:41:13 -0500

    Update Principal.java

--- Practica 02/Act1_7/Principal.java
+++ Practica 02/Act1_7/Principal.java
@@ -3,7 +3,7 @@ import Act1.*;
 import java.util.*;
 public class Principal{
     public static void main(String [] args){
-        Bolsa<Chocolatina> bolsaCho=new Bolsa<>(3);
+        Bolsa<Chocolatina> bolsaCho = new Bolsa<>(3);
         Chocolatina c1=new Chocolatina("milky");
         Chocolatina c2=new Chocolatina("Iberica");
         Chocolatina c3=new Chocolatina("Valenzuela");
