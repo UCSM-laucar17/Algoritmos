4eba37df3de0c8a2870f7413e9687f0c12141282
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-05-11 19:19:47 -0500

    Create InOrder.java

---
+++ Practica 07/Act07/InOrder.java
@@ -0,0 +1,19 @@
+package Practica.Act07;
+import Practica.Act06.*;
+public class InOrder{
+    private Node root;
+    public InOrder(){
+        this.root = null;
+    }
+    public void inOrder(){
+        System.out.print(" In Order: ");
+        inOrder(this.root);
+    }
+    private void inOrder(Node actual){
+        if (actual != null) {
+            inOrder(actual.left);
+            System.out.print(actual.dato + " ");
+            inOrder(actual.right);
+        }
+    }
+}
